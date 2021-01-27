86b240d37210c623ab98bc1667b0a51081982d1e
Santiago Galeano <santiagogaleano@MacBook-Pro-de-Santiago.local>
2021-01-26 23:26:35 -0500

    first commit

---
+++ app/db.py
@@ -0,0 +1,13 @@
+import os
+
+
+POSTGRESQL = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Hades',
+        'USER': 'postgres',
+        'PASSWORD': 'santiago',
+        'HOST': 'localhost',
+        'PORT': '5432'
+    }
+}
